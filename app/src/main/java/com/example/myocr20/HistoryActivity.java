43445d9d83bad1cabfac98219b44e747817a2fe1
Ketan <ktanqwerty@gmail.com>
2019-10-06 20:54:48 +0530

    l

--- app/src/main/java/com/example/myocr20/HistoryActivity.java
+++ app/src/main/java/com/example/myocr20/HistoryActivity.java
@@ -17,15 +17,13 @@ import android.widget.ListView;
 import java.util.ArrayList;
 
 public class HistoryActivity extends AppCompatActivity {
-    ListView listView;
-    SQLiteDatabase myDatabase;
-    ArrayList<String> arrayList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
-          listView = (ListView) findViewById(R.id.list_view);
-      arrayList = new ArrayList<String>();
+        ListView listView = (ListView) findViewById(R.id.list_view);
+        ArrayList<String> arrayList = new ArrayList<String>();
 
         String scannedtextt;
         Intent intent4 = getIntent();
@@ -34,12 +32,20 @@ public class HistoryActivity extends AppCompatActivity {
         try {
 
 
-             myDatabase = this.openOrCreateDatabase("Users", MODE_PRIVATE, null);
-            myDatabase.execSQL("CREATE TABLE IF NOT EXISTS users (name VARCHAR,id INTEGER PRIMARY KEY  AUTOINCREMENT )");
+            SQLiteDatabase myDatabase = this.openOrCreateDatabase("Users", MODE_PRIVATE, null);
+            myDatabase.execSQL("CREATE TABLE IF NOT EXISTS users (name VARCHAR,id INTEGER  )");
             if(scannedtextt !=null) {
-                myDatabase.execSQL("INSERT INTO users(name,age) VALUES ('" +scannedtextt+ "'");
+                myDatabase.execSQL("INSERT INTO users(name,age) VALUES ('" +scannedtextt+ "',3)");
             }
-            display();
+            Cursor c = myDatabase.rawQuery("SELECT * FROM users", null);
+            int nameindex = c.getColumnIndex("name");
+            int ageindex = c.getColumnIndex("age");
+            c.moveToFirst();
+            while (c != null) {
+                Log.i("name", c.getString(nameindex));
+                arrayList.add(c.getString(nameindex));
+                c.moveToNext();
+            }
 
         }
 
@@ -49,46 +55,9 @@ public class HistoryActivity extends AppCompatActivity {
         }
 
 
-
-        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
-                new AlertDialog.Builder(HistoryActivity.this)
-                        .setIcon(android.R.drawable.ic_dialog_alert)
-                        .setTitle("Are you sure")
-                        .setMessage("Do you want to Delete this Text")
-                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                //perform here
-                                myDatabase.execSQL("DELETE FROM users WHERE id = i");
-                                //arrayAdapter.notifyDataSetChanged();
-                                arrayList.clear();
-                                display();
-                            }
-                        })
-                        .setNegativeButton("No",null)
-                        .show();
-                return true;
-            }
-        });
-
-
-
-    }
-    public void display(){
-        Cursor c = myDatabase.rawQuery("SELECT * FROM users", null);
-        int nameindex = c.getColumnIndex("name");
-
-        c.moveToFirst();
-        while (c != null) {
-
-            arrayList.add(c.getString(nameindex));
-            c.moveToNext();
-        }
-
         arrayList.add("fds");
-        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,arrayList);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,arrayList);
         listView.setAdapter(arrayAdapter);
+
     }
 }
